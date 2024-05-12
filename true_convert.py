58e2514ac5b1db937341545e469e256bf5bb7dc4
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-05-12 13:04:41 +0300

    feat: output to only other existing notebook besides the input one

--- true_convert.py
+++ true_convert.py
@@ -1,15 +1,17 @@
 import json
 import argparse
+import os
 
-# TODO: output to only other existing notebook besides the input one
 parser = argparse.ArgumentParser(description='Output proper notebook.')
 parser.add_argument('-i', '--input', type=str, help='notebook to be processed.')
-parser.add_argument('-o', '--output', type=str, help='output name of notebook.')
+parser.add_argument('-o', '--output', type=str, required=False, help='output name of notebook.')
 
 args = parser.parse_args()
 
 if '.ipynb' not in args.input: args.input += '.ipynb'
-if '.ipynb' not in args.output: args.output += '.ipynb'
+if args.output:
+    if '.ipynb' not in args.output:
+        args.output += '.ipynb'
 
 with open(args.input, 'r') as file:
     data = file.read()
@@ -26,20 +28,27 @@ for cell in data_j['cells']:
 
         for idx in range(len(cell['source'])):
             if cell['source'][idx] != '\n':
-                print(f'idx: {idx}, line: {cell["source"][idx]}')
                 cell['source'] = cell['source'][idx:]
                 break
 
         for idx in reversed(range(len(cell['source']))):
             if cell['source'][idx] != '\n':
-                print(f'idx: {idx}, line: {cell["source"][idx]}')
                 cell['source'] = cell['source'][:idx+1]
                 break
 
         cell['source'][-1] = cell['source'][-1].strip()
 
 
-with open(args.output, 'w') as result:
-    result.write(json.dumps(data_j))
+if not args.output:
+    notebook_files = [file for file in os.listdir() if '.ipynb' in file]
+    assert len(notebook_files) == 2, 'You can omit a destination notebook for the output if there are exactly two notebooks in the directory. In other cases please specify a destination notebook with the -o flag.'
+    for file in notebook_files:
+        if file != args.input:
+            destination_notebook = file
+    with open(destination_notebook, 'w') as result:
+        result.write(json.dumps(data_j))
+else:
+    with open(args.output, 'w') as result:
+        result.write(json.dumps(data_j))
 
 
